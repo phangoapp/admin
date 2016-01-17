70319a4fe2a04ca7950da314945a9a063a20e0fa
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-01-17 16:33:32 +0100

    Now, the url rewrite use ADMIN_FOLDER constant

--- urls.php
+++ urls.php
@@ -4,6 +4,6 @@ use PhangoApp\PhaRouter\Routes;
 
 //Routes::$urls['welcome\/([0-9]+)\/(\w+)']=array('index', 'page');
 
-Routes::$urls['admin\/(\w+)']=array('index', 'home');
+Routes::$urls[ADMIN_FOLDER.'\/(\w+)']=array('index', 'home');
 
 ?>
